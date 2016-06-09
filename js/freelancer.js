890a9f8117c8e1e9c1d42c95595a8c18eeeed741
Gregory Emirian <gregory@monkeypatch.io>
2016-06-09 13:56:52 +0200

    closing modal using back button, nav styling, modal styling

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,19 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
+
+$('div.modal').on('hide', function() {
+	var hash = this.id;
+	history.pushState('', document.title, window.location.pathname);
+});
